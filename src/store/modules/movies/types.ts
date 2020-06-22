ba98dfdb12206577a84f5661e2915b624a4d20d5
truedlfn <dmi.villione@gmail.com>
2020-06-22 22:21:29 +0300

    add movies types

--- src/store/modules/movies/types.ts
+++ src/store/modules/movies/types.ts
@@ -1,77 +1,111 @@
 // Movie details
 type TBelongsToCollection = {
-  id: number;
-  name: string;
-  poster_path: string;
-  backdrop_path: string;
-};
+  id: number
+  name: string
+  poster_path: string
+  backdrop_path: string
+}
 type TProductionCompanies = {
-  name: string;
-  id: number;
-  logo_path: string | null;
-  origin_country: string;
-};
+  name: string
+  id: number
+  logo_path: string | null
+  origin_country: string
+}
 type TProductionCountries = {
-  iso_3166_1: string;
-  name: string;
-};
+  iso_3166_1: string
+  name: string
+}
 type TSpokenLanguages = {
-  iso_639_1: string;
-  name: string;
-};
+  iso_639_1: string
+  name: string
+}
 
 type TGenres = {
-  id: number;
-  name: string;
-};
+  id: number
+  name: string
+}
 export type TMovieDetails = {
-  adult: boolean;
-  backdrop_path: string | null;
-  belongs_to_collection: null | TBelongsToCollection;
-  budget: number;
-  genres: Array<TGenres>;
-  homepage: string | null;
-  id: number;
-  imdb_id: string | null;
-  original_language: string;
-  original_title: string;
-  overview: string;
-  popularity: string;
-  poster_path: string | null;
-  production_companies: Array<TProductionCompanies>;
-  production_countries: Array<TProductionCountries>;
-  release_date: string;
-  revenue: number;
-  runtime: number | null;
-  spoken_languages: Array<TSpokenLanguages>;
-  status: string;
-  tagline: string | null;
-  title: string;
-  video: boolean;
-  vote_average: number;
-  vote_count: number;
-};
+  adult: boolean
+  backdrop_path: string | null
+  belongs_to_collection: null | TBelongsToCollection
+  budget: number
+  genres: Array<TGenres>
+  homepage: string | null
+  id: number
+  imdb_id: string | null
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: string
+  poster_path: string | null
+  production_companies: Array<TProductionCompanies>
+  production_countries: Array<TProductionCountries>
+  release_date: string
+  revenue: number
+  runtime: number | null
+  spoken_languages: Array<TSpokenLanguages>
+  status: string
+  tagline: string | null
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
 
+type TMovie = {
+  poster_path: string | null
+  adult: boolean
+  overview: string
+  release_date: string
+  genre_ids: Array<number>
+  id: number
+  original_title: string
+  original_language: string
+  title: string
+  backdrop_path: string | null
+  popularity: number
+  vote_count: string
+  video: boolean
+  vote_average: number
+}
 // Popular movies
 
 export type TPopularMovies = {
-  page: number;
-  results: Array<{
-    poster_path: string | null;
-    adult: boolean;
-    overview: string;
-    release_date: string;
-    genre_ids: Array<number>;
-    id: number;
-    original_title: string;
-    original_language: string;
-    title: string;
-    backdrop_path: string | null;
-    popularity: number;
-    vote_count: string;
-    video: boolean;
-    vote_average: number;
-  }>;
-  total_results: number;
-  total_pages: number;
-};
+  page: number
+  results: Array<TMovie>
+  total_results: number
+  total_pages: number
+}
+// Now playing movies
+export type TNowPlayingMovies = {
+  page: number
+  results: Array<TMovie>
+  dates: {
+    maximum: string
+    minimum: string
+  }
+  total_results: number
+  total_pages: number
+}
+
+// Top rated movies
+
+export type TTopRatedMovies = {
+  page: number
+  results: Array<TMovie>
+  total_results: number
+  total_pages: number
+}
+
+// Upcoming movies
+
+export type TUpcomingMovies = {
+  page: number
+  results: Array<TMovie>
+  dates: {
+    maximum: string
+    minimum: string
+  }
+  total_results: number
+  total_pages: number
+}
