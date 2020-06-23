96114d0c124fdda67885ddf3766b70a3b8dcab9d
truedlfn <dmi.villione@gmail.com>
2020-06-23 22:17:49 +0300

    add movies selectors

--- src/store/modules/movies/selectors.ts
+++ src/store/modules/movies/selectors.ts
@@ -1,3 +1,15 @@
-import { AppStateType } from '@src/store';
+import { AppStateType } from '@src/store'
 
-export const getMovieDetails = (state: AppStateType) => state.movies.movieDetails
+export const getMovieDetails = (state: AppStateType) =>
+  state.movies.currentMovie.details
+export const getSimilarMovies = (state: AppStateType) =>
+  state.movies.currentMovie.similar
+export const getRecommendations = (state: AppStateType) =>
+  state.movies.currentMovie.recommendations
+
+export const getPopularMovies = (state: AppStateType) =>
+  state.movies.popularMovies
+export const getNowPlayingMovies = (state: AppStateType) =>
+  state.movies.nowPlayingMovies
+export const getTopRatedMovies = (state: AppStateType) =>
+  state.movies.topRatedMovies
