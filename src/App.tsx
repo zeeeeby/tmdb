f2f6580cbb02242088eaca00fe97c3c4efebb64f
truedlfn <dmi.villione@gmail.com>
2020-06-23 22:16:06 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react'
+import { Switch, Route } from 'react-router-dom'
 
-import Container from '@material-ui/core/Container';
+import Container from '@material-ui/core/Container'
 
-import { Header } from '@src/components/Header';
-import { Auth, Movies, NowPlaying } from '@src/pages';
+import { Header } from '@src/components/Header'
+import { Auth, Movies, NowPlaying } from '@src/pages'
 
-import { account } from '@src/store/modules/account';
-import { auth } from '@src/store/modules/auth';
-import { localStorage } from './lib/local-storage';
+import { account } from '@src/store/modules/account'
+import { auth } from '@src/store/modules/auth'
+import { localStorage } from './lib/local-storage'
 
 export const App = () => {
-  const { getProfile } = account.useActions();
-  const { updateAuthStatus } = auth.useActions();
+  const { getProfile } = account.useActions()
+  const { updateAuthStatus } = auth.useActions()
   useEffect(() => {
-    const f = async () => {
+    const tryToGetProfile = async () => {
       try {
-        const session_id: string = await localStorage.load('session')
-          .session_id;
+        const session_id: string = await localStorage.load('session').session_id
 
         if (session_id) {
-          await getProfile();
-          await updateAuthStatus(true);
+          await getProfile()
+          await updateAuthStatus(true)
         }
       } catch {}
-    };
-    f();
-  }, []);
+    }
+    tryToGetProfile()
+  }, [])
   return (
     <div>
       <Header />
@@ -39,5 +38,5 @@ export const App = () => {
         </Switch>
       </Container>
     </div>
-  );
-};
+  )
+}
