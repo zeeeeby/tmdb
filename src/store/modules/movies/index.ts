a4685c31d20fd9c6d92d0a53aeba013e29162bb1
truedlfn <dmi.villione@gmail.com>
2020-06-23 22:18:04 +0300

    add movies hooks api

--- src/store/modules/movies/index.ts
+++ src/store/modules/movies/index.ts
@@ -1,4 +1,26 @@
-import { moviesReducer } from './reducer';
-import { useActions } from '@src/hooks/useActions';
+import { useSelector } from 'react-redux'
 
-export { moviesReducer };
+import { moviesReducer } from './reducer'
+import { useActions } from '@src/hooks/useActions'
+import {
+  getMovieDetails,
+  getNowPlayingMovies,
+  getPopularMovies,
+  getRecommendations,
+  getSimilarMovies,
+  getTopRatedMovies,
+} from './selectors'
+
+export { moviesReducer }
+
+export const movies = {
+  currentMovie: {
+    useDetails: () => useSelector(getMovieDetails),
+    useSimilar: () => useSelector(getSimilarMovies),
+    useRecommendations: () => useSelector(getRecommendations),
+  },
+  usePopular: () => useSelector(getPopularMovies),
+  useNowPlayings: () => useSelector(getNowPlayingMovies),
+  useTopRated: () => useSelector(getTopRatedMovies),
+  useActions: () => null,
+}
