b6aaa18d6f3fbf1f9923f5a943d29ae4da992b9d
truedlfn <dmi.villione@gmail.com>
2020-06-24 22:26:05 +0300

    add tv hooks api

--- src/store/modules/tv/index.ts
+++ src/store/modules/tv/index.ts
@@ -1,1 +1,27 @@
-export const f = ()=>null
+import { useSelector } from 'react-redux'
+import { useActions } from '@src/hooks/useActions'
+import { actions, tvReducer } from './reducer'
+import {
+  getAiringTodayTV,
+  getOnTheAirTV,
+  getPopularTV,
+  getRecommendations,
+  getSimilarTV,
+  getTVDetails,
+  getTopRatedTVs,
+} from './selectors'
+
+export { tvReducer }
+
+export const tv = {
+  currentTV: {
+    useDetails: () => useSelector(getTVDetails),
+    useSimilar: () => useSelector(getSimilarTV),
+    useRecommendations: () => useSelector(getRecommendations),
+  },
+  usePopular: () => useSelector(getPopularTV),
+  useAiringToday: () => useSelector(getAiringTodayTV),
+  useOnTheAir: () => useSelector(getOnTheAirTV),
+  useTopRated: () => useSelector(getTopRatedTVs),
+  useActions: (): typeof actions => useActions(actions),
+}
