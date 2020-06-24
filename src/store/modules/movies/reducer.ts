015c1f51c941d7cc76e19b6f1aaf6380c8b023cd
truedlfn <dmi.villione@gmail.com>
2020-06-24 22:22:52 +0300

    add new functions to movies reducer

--- src/store/modules/movies/reducer.ts
+++ src/store/modules/movies/reducer.ts
@@ -1,6 +1,7 @@
 import { InferActionsTypes, BaseThunkType } from '../..'
 
 import { Dispatch } from 'redux'
+import { moviesApi } from '@src/api'
 
 import {
   TMovieDetails,
@@ -9,7 +10,9 @@ import {
   TTopRatedMovies,
   TSimilarMovies,
   TRecommendations,
+  TUpcomingMovies,
 } from './types'
+import { useDispatch } from 'react-redux'
 
 type ActionsTypes = InferActionsTypes<typeof localActions>
 type ThunkType = BaseThunkType<ActionsTypes>
@@ -24,6 +27,7 @@ let initialState = {
   popularMovies: {} as TPopularMovies,
   nowPlayingMovies: {} as TNowPlayingMovies,
   topRatedMovies: {} as TTopRatedMovies,
+  upcomingMovies: {} as TUpcomingMovies,
 }
 
 export const moviesReducer = (
@@ -64,6 +68,8 @@ export const moviesReducer = (
       return { ...state, nowPlayingMovies: action.payload.movies }
     case 'tmdb/movies/SET_TOP_RATED_MOVIES':
       return { ...state, topRatedMovies: action.payload.movies }
+    case 'tmdb/movies/SET_UPCOMING_MOVIES':
+      return { ...state, upcomingMovies: action.payload.movies }
     default:
       return state
   }
@@ -85,7 +91,7 @@ const localActions = {
       type: 'tmdb/movies/SET_NOW_PLAYING_MOVIES',
       payload: { movies },
     } as const),
-  setTopRatedMovies: (movies: TNowPlayingMovies) =>
+  setTopRatedMovies: (movies: TTopRatedMovies) =>
     ({
       type: 'tmdb/movies/SET_TOP_RATED_MOVIES',
       payload: { movies },
@@ -100,4 +106,118 @@ const localActions = {
       type: 'tmdb/movies/SET_RECOMMENDATIONS',
       payload: { movies },
     } as const),
+  setUpcoming: (movies: TUpcomingMovies) =>
+    ({
+      type: 'tmdb/movies/SET_UPCOMING_MOVIES',
+      payload: { movies },
+    } as const),
+}
+
+const getMovieDetails = (movie_id: number): ThunkType => async (
+  dispatch: Dispatch<ActionsTypes>
+) => {
+  try {
+    dispatch(localActions.setMovieDetails(await moviesApi.getDetails(movie_id)))
+  } catch (error) {
+    throw error.response
+  }
+}
+const getPopularMovies = (
+  language?: string,
+  page?: number,
+  region?: string
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setPopularMovies(
+        await moviesApi.getPopular(language, page, region)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+const getNowPlayingMovies = (
+  language?: string,
+  page?: number,
+  region?: string
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setNowPlayingMovies(
+        await moviesApi.getNowPlaying(language, page, region)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+const getTopRatedMovies = (
+  language?: string,
+  page?: number,
+  region?: string
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setTopRatedMovies(
+        await moviesApi.getTopRated(language, page, region)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+const getSimilarMovies = (
+  movie_id: number,
+  language?: string,
+  page?: number
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setSimilarMovies(
+        await moviesApi.getSimilar(movie_id, language, page)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+const getRecommendations = (
+  movie_id: number,
+  language?: string,
+  page?: number
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setRecommendations(
+        await moviesApi.getRecommendations(movie_id, language, page)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+const getUpcomingMovies = (
+  language?: string,
+  page?: number,
+  region?: string
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setUpcoming(
+        await moviesApi.getUpcoming(language, page, region)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+export const actions = {
+  getMovieDetails,
+  getTopRatedMovies,
+  getPopularMovies,
+  getRecommendations,
+  getSimilarMovies,
+  getNowPlayingMovies,
+  getUpcomingMovies,
 }
