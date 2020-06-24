144af3b9857df8886367c268fe55d0a784943f03
truedlfn <dmi.villione@gmail.com>
2020-06-24 22:26:16 +0300

    add tv selectors

--- src/store/modules/tv/selectors.ts
+++ src/store/modules/tv/selectors.ts
@@ -1,1 +1,11 @@
-export const f = () => null
+import { AppStateType } from '@src/store'
+
+export const getTVDetails = (state: AppStateType) => state.tv.currentTV.details
+export const getSimilarTV = (state: AppStateType) => state.tv.currentTV.similar
+export const getRecommendations = (state: AppStateType) =>
+  state.tv.currentTV.recommendations
+
+export const getPopularTV = (state: AppStateType) => state.tv.popularTV
+export const getOnTheAirTV = (state: AppStateType) => state.tv.onTheAirTV
+export const getAiringTodayTV = (state: AppStateType) => state.tv.airingTodayTV
+export const getTopRatedTVs = (state: AppStateType) => state.tv.topRatedTV
