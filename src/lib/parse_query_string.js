55d5fcc87799e29b159d32fbb8739ca49a4ab764
truedlfn <dmi.villione@gmail.com>
2020-07-14 20:57:35 +0300

    add parse_query_string function

---
+++ src/lib/parse_query_string.js
@@ -0,0 +1,8 @@
+export const parseQueryString = (str) =>
+  str
+    .substring(1)
+    .split('&')
+    .reduce((acc, el) => {
+      const [key, value] = el.split('=')
+      return { ...acc, [key]: value }
+    }, {})
