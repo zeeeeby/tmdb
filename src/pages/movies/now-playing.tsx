a791e7de0b87a1db5aa896ed40de91c9fb4ea851
truedlfn <dmi.villione@gmail.com>
2020-07-27 23:03:07 +0300

    add movies/now-playing page

---
+++ src/pages/movies/now-playing.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Pagination from '@material-ui/lab/Pagination'
+import { CardsList } from '@src/components/CardsList'
+import { useHistory, useLocation } from 'react-router-dom'
+import { movies } from '@src/store/modules/movies'
+import { parseQueryString } from '@src/lib/parse_query_string'
+const useStyles = makeStyles({
+  pagination: {
+    '& ul': { justifyContent: 'center', margin: "10px 0" },
+  },
+})
+export const NowPlaying: React.FC = () => {
+  const classes = useStyles()
+  const nowPlayingMovies = movies.useNowPlaying()
+  const history = useHistory()
+  const params = useLocation()
+  let pageNumber = parseQueryString(params.search).page || 1
+
+  const [page, setPage] = React.useState(parseInt(pageNumber))
+  const { getNowPlayingMovies } = movies.useActions()
+
+  const switchPage = (page: number) => {
+    setPage(page)
+    history.push(`?page=${page}`)
+  }
+
+  React.useEffect(() => {
+    //@ts-ignore
+    getNowPlayingMovies('', page).catch((err: any) => {
+      if (err?.status === 422) switchPage(1)
+    })
+  }, [page])
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    switchPage(value)
+  }
+  return (
+    <>
+      <CardsList cards={nowPlayingMovies?.results} cardType="movie" />
+      <Pagination
+        className={classes.pagination}
+        count={nowPlayingMovies?.total_pages}
+        page={page}
+        color="primary"
+        size="large"
+        onChange={handleChange}
+      />
+    </>
+  )
+}
