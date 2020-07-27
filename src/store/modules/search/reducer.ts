b70505baf7f8a06f61d9407883a96f9c050d3183
truedlfn <dmi.villione@gmail.com>
2020-07-27 23:01:05 +0300

    add search/reducer

---
+++ src/store/modules/search/reducer.ts
@@ -0,0 +1,54 @@
+import { InferActionsTypes, BaseThunkType } from '../..'
+
+import { Dispatch } from 'redux'
+import { searchApi } from '@src/api'
+
+import { TSearchResult } from './types'
+
+type ActionsTypes = InferActionsTypes<typeof localActions>
+type ThunkType = BaseThunkType<ActionsTypes>
+type TInitialState = typeof initialState
+
+let initialState = {
+  searchResults: {} as TSearchResult | null,
+}
+
+export const searchReducer = (
+  state = initialState,
+  action: ActionsTypes
+): TInitialState => {
+  switch (action.type) {
+    case 'tmdb/search/SET_SEARCH_RESULT':
+      return { ...state, searchResults: action.payload.results }
+    default:
+      return state
+  }
+}
+
+const localActions = {
+  setSearchResult: (results: TSearchResult | null) =>
+    ({
+      type: 'tmdb/search/SET_SEARCH_RESULT',
+      payload: { results },
+    } as const),
+}
+
+const find = (
+  query: string,
+  page?: number,
+  language?: string
+): ThunkType => async (dispatch: Dispatch<ActionsTypes>) => {
+  try {
+    dispatch(
+      localActions.setSearchResult(
+        await searchApi.search(query, page, language)
+      )
+    )
+  } catch (error) {
+    throw error.response
+  }
+}
+
+export const actions = {
+  find,
+}
