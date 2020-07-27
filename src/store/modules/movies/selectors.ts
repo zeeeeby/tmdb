da38e6d7871179dc6d7010f8f916769630e1b30d
truedlfn <dmi.villione@gmail.com>
2020-07-27 23:01:36 +0300

    upd movies/selectors

--- src/store/modules/movies/selectors.ts
+++ src/store/modules/movies/selectors.ts
@@ -6,11 +6,14 @@ export const getSimilarMovies = (state: AppStateType) =>
   state.movies.currentMovie.similar
 export const getRecommendations = (state: AppStateType) =>
   state.movies.currentMovie.recommendations
-
+export const getVideos = (state: AppStateType) =>
+  state.movies.currentMovie.videos
+  
 export const getPopularMovies = (state: AppStateType) =>
   state.movies.popularMovies
 export const getNowPlayingMovies = (state: AppStateType) =>
   state.movies.nowPlayingMovies
 export const getTopRatedMovies = (state: AppStateType) =>
   state.movies.topRatedMovies
-
+export const getUpcomingMovies = (state: AppStateType) =>
+  state.movies.upcomingMovies
